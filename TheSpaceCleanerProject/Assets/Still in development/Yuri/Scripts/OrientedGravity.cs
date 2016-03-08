b8e0a13eeb38d72770134d04aa438ca757c8175e
Yuri Fernando <yuri.m.g74@gmail.com>
2016-03-08 01:51:12 -0300

    Adicionado Prototipo de Gravidade

---
+++ TheSpaceCleanerProject/Assets/Still in development/Yuri/Scripts/OrientedGravity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrientedGravity : MonoBehaviour {
+
+    public GameObject refObject;
+    public float gravityForce;
+    public Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        ApplyGravity();
+	}
+
+    void ApplyGravity() {
+        RaycastHit hit;
+        Vector3 dir = refObject.transform.position - transform.position;
+        if (Physics.Raycast(transform.position, dir,out hit, Mathf.Infinity)) {
+            rb.AddForce(new Vector3(
+                hit.point.x*-gravityForce,
+                hit.point.y*-gravityForce,
+                hit.point.z*-gravityForce), ForceMode.Force);
+           //transform.up = hit.normal;
+            print(hit.point);
+        }
+        else{
+            print("Passou reto");
+        }
+        
+    }
+}
